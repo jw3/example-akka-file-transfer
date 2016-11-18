ad5b4d75c5b61b23334630c5a0fc6feb807070d9
John Wass <jwass3@gmail.com>
2016-11-18 05:30:38 -0500

    cleanup

--- src/main/scala/com/github/jw3/Streams.scala
+++ src/main/scala/com/github/jw3/Streams.scala
@@ -4,27 +4,16 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.scaladsl.Flow
+import com.github.jw3.dest.{conf ⇒ destcfg}
+import com.github.jw3.pipe.{conf ⇒ pipecfg}
+import com.github.jw3.source.{conf ⇒ sourcecfg}
 
 object streams {
   type Connection = Flow[HttpRequest, HttpResponse, _]
 
-  def connection(host: String, port: Int = 8080, ssl: Boolean = false)(implicit system: ActorSystem): Connection = {
-    if (ssl) Http().outgoingConnectionHttps(host, port)
-    else Http().outgoingConnection(host, port)
-  }
+  def source(implicit system: ActorSystem) = connection(sourcecfg.host, sourcecfg.port)
+  def pipe(implicit system: ActorSystem) = connection(pipecfg.host, pipecfg.port)
+  def dest(implicit system: ActorSystem) = connection(destcfg.host, destcfg.port)
 
-  def source(implicit system: ActorSystem): Connection = {
-    import com.github.jw3.source.conf
-    connection(conf.host, conf.port)
-  }
-
-  def pipe(implicit system: ActorSystem): Connection = {
-    import com.github.jw3.pipe.conf
-    connection(conf.host, conf.port)
-  }
-
-  def dest(implicit system: ActorSystem): Connection = {
-    import com.github.jw3.dest.conf
-    connection(conf.host, conf.port)
-  }
+  def connection(host: String, port: Int = 8080)(implicit system: ActorSystem): Connection = Http().outgoingConnection(host, port)
 }
