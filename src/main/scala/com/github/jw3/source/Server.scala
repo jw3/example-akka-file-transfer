2a0f5a1c726798ff104fbb3c92fddaf98d3935b7
John Wass <jwass3@gmail.com>
2016-11-23 17:10:48 -0500

    add slow source

--- src/main/scala/com/github/jw3/source/Server.scala
+++ src/main/scala/com/github/jw3/source/Server.scala
@@ -2,8 +2,11 @@ package com.github.jw3.source
 
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.RouteDirectives.{complete ⇒ _}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.FileIO
 import akka.util.Timeout
 import com.github.jw3._
 
@@ -36,6 +39,12 @@ class Server(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
     logRequest("---SOURCE---") {
       path(source.conf.path) {
         getFromFile(source.conf.file)
+      } ~
+      path("slow") {
+        complete {
+          HttpEntity.Default(ContentTypes.`text/plain(UTF-8)`, source.conf.file.length,
+            FileIO.fromPath(source.conf.file.toPath, 1).map { v ⇒ Thread.sleep(100); v })
+        }
       }
     }
   }
