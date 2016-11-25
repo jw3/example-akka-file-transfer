09917b13c0650389e0da0ba85ce5a40e385bb36f
John Wass <jwass3@gmail.com>
2016-11-24 21:41:52 -0500

    tap the piped stream

--- src/main/scala/com/github/jw3/pipe/Server.scala
+++ src/main/scala/com/github/jw3/pipe/Server.scala
@@ -1,14 +1,17 @@
 package com.github.jw3.pipe
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.Done
+import akka.actor.{Actor, ActorLogging, ActorRef, PoisonPill, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.{HttpEntity, _}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
-import akka.util.Timeout
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.util.{ByteString, Timeout}
 import com.github.jw3._
+import com.github.jw3.pipe.Server._
+import com.github.jw3.pipe.Tap.Initialize
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
@@ -16,6 +19,11 @@ import scala.util.{Failure, Success}
 
 object Server {
   def props(implicit mat: ActorMaterializer) = Props(new Server)
+
+  def size(r: HttpResponse) = r.entity.contentLengthOption match {
+    case Some(sz) ⇒ sz
+    case _ ⇒ throw new RuntimeException("couldnt extract entity size")
+  }
 }
 
 class Server(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
@@ -39,8 +47,11 @@ class Server(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
     logRequest("---PIPE---") {
       pathPrefix(pipe.conf.path) {
         get {
+          val tap = context.actorOf(Tap.props)
+          // need to tap this transfer with a actor as a listener to provide status to webhook endpoints
           val f = Source.single(HttpRequest(uri = source.conf.uri)).via(streams.source)
-                  .map(r ⇒ Multipart.FormData(Multipart.FormData.BodyPart(source.conf.filename, HttpEntity(ContentTypes.`text/plain(UTF-8)`, size(r), r.entity.dataBytes), Map("filename" → source.conf.filename))))
+                  .alsoTo(Sink.foreach(r ⇒ tap ! Initialize(size(r))))
+                  .map(r ⇒ Multipart.FormData(Multipart.FormData.BodyPart(source.conf.filename, HttpEntity(ContentTypes.`text/plain(UTF-8)`, size(r), r.entity.dataBytes.via(Tap.to(tap))), Map("filename" → source.conf.filename))))
                   .mapAsync(1)(r ⇒
                     Source.fromFuture(Marshal(r).to[RequestEntity])
                     .map(e ⇒ HttpRequest(method = HttpMethods.POST, uri = dest.conf.uri, entity = e))
@@ -54,12 +65,34 @@ class Server(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
     }
   }
 
-  def size(r: HttpResponse) = r.entity.contentLengthOption match {
-    case Some(sz) ⇒ sz
-    case _ ⇒ throw new RuntimeException("couldnt extract entity size")
-  }
-
   def receive: Receive = {
     case _ ⇒
+  }
+}
+
+object Tap {
+  def props = Props(new Tap)
+  def to(tap: ActorRef) = Flow[ByteString].alsoTo(Sink.actorRef(tap, Done))
+
+  case class Initialize(sz: Long)
+}
+
+class Tap extends Actor {
+  def receive: Receive = {
+    case Initialize(sz) ⇒ context.become(ready(sz))
+  }
+
+  def ready(sz: Long): Receive = {
+    var pos: Long = 0
+
+    {
+      case v: ByteString ⇒
+        pos += v.length
+        println(s"tapped[${v.utf8String}] [$pos/$sz]")
+
+      case Done ⇒
+        println("----TAP Complete----")
+        self ! PoisonPill
+    }
   }
 }
