414c70ccff0c41045ac7a5371660b415459d1a4e
John Wass <jwass3@gmail.com>
2016-11-26 21:51:23 -0500

    Tap is an ActorPublisher

--- src/main/scala/com/github/jw3/pipe/Server.scala
+++ src/main/scala/com/github/jw3/pipe/Server.scala
@@ -10,8 +10,10 @@ import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.model.{HttpEntity, _}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
+import akka.stream.actor.ActorPublisher
+import akka.stream.actor.ActorPublisherMessage.Cancel
 import akka.stream.scaladsl.{Flow, Sink, Source}
-import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.util.{ByteString, Timeout}
 import com.github.jw3._
 import com.github.jw3.pipe.Server._
@@ -62,7 +64,7 @@ class Server(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
             extractUpgradeToWebSocket { upgrade ⇒
               complete {
                 val sink = Sink.actorRef[Message](stream, Mute(stream))
-                val source = Source.actorRef[TextMessage](10, OverflowStrategy.fail).mapMaterializedValue(stream ! Listen(_))
+                val source = Source.fromPublisher(ActorPublisher[String](stream)).map(TextMessage(_))
                 upgrade.handleMessages(Flow.fromSinkAndSource(sink, source))
               }
             }
@@ -76,17 +78,16 @@ class Server(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
     import context.system
 
     val id = UUID.randomUUID.toString.take(7)
-    println(s"creating stream: $id")
-    val tap = context.actorOf(Tap.props, "abc")
-    // need to tap this transfer with a actor as a listener to provide status to webhook endpoints
-    val f = Source.single(HttpRequest(uri = src)).via(streams.source)
-            .alsoTo(Sink.foreach(r ⇒ tap ! Initialize(size(r))))
-            .map(r ⇒ Multipart.FormData(Multipart.FormData.BodyPart(source.conf.filename, HttpEntity(ContentTypes.`text/plain(UTF-8)`, size(r), r.entity.dataBytes.via(Tap.to(tap))), Map("filename" → source.conf.filename))))
-            .mapAsync(1)(r ⇒ Marshal(r).to[RequestEntity].map(e ⇒ HttpRequest(method = HttpMethods.POST, uri = dest.conf.uri, entity = e)))
-            .via(streams.dest)
-            .runWith(Sink.head)
+    val tap = context.actorOf(Tap.props, id)
 
-    complete(f)
+    Source.single(HttpRequest(uri = src)).via(streams.source)
+    .alsoTo(Sink.foreach(r ⇒ tap ! Initialize(size(r))))
+    .map(r ⇒ Multipart.FormData(Multipart.FormData.BodyPart(source.conf.filename, HttpEntity(ContentTypes.`text/plain(UTF-8)`, size(r), r.entity.dataBytes.via(Tap.to(tap))), Map("filename" → source.conf.filename))))
+    .mapAsync(1)(r ⇒ Marshal(r).to[RequestEntity].map(e ⇒ HttpRequest(method = HttpMethods.POST, uri = dest.conf.uri, entity = e)))
+    .via(streams.dest)
+    .runWith(Sink.head)
+
+    complete(s"""{"id":"$id"}""")
   }
 
   def extractStream(streamId: String)(fn: ActorRef ⇒ Route)(implicit to: Timeout) = {
@@ -112,37 +113,30 @@ object Tap {
   case class Mute(ref: ActorRef)
 }
 
-class Tap extends Actor {
+class Tap extends ActorPublisher[String] {
   def receive: Receive = {
     case Initialize(sz) ⇒ context.become(ready(sz))
   }
 
   def ready(sz: Long): Receive = {
-    var listeners = Seq[ActorRef]()
     var pos: Long = 0
 
     {
+      case Cancel =>
+        context.stop(self)
+
       case v: ByteString ⇒
         pos += v.length
-
         val message = s"tapped[${v.utf8String}] [$pos/$sz]"
-        println(message)
-        listeners.foreach { l ⇒
-          println("sending to listener")
-          l ! TextMessage(message)
+
+        if (totalDemand > 0) {
+          println(s"sending: $message")
+          onNext(message)
         }
 
       case TapComplete ⇒
         println("----TAP Complete----")
         self ! PoisonPill
-
-      case Listen(ref) ⇒
-        println("----Listen----")
-        listeners ++= Seq(ref)
-
-      case Mute(ref) ⇒
-        println("----Mute----")
-        listeners = listeners.filterNot(_ == ref)
     }
   }
 }
